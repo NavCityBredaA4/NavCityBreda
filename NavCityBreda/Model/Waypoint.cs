d2ae8ffe19ee6e912c2cc942180c884a77300b69
Kenneth van Ewijk <kennyboy@live.nl>
2015-12-16 16:45:44 +0100

    Setup to adding waypoints and landmarks

--- NavCityBreda/Model/Waypoint.cs
+++ NavCityBreda/Model/Waypoint.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
 
 namespace NavCityBreda.Model
 {
     class Waypoint
     {
+        Geopoint location;
+
+        public Waypoint(Geopoint p)
+        {
+            location = p;
+        }
+
+        public Waypoint(int a, int la, int lo)
+        {
+            location = new Geopoint(new BasicGeoposition() { Altitude = a, Latitude = la, Longitude = lo });
+        }
     }
 }
